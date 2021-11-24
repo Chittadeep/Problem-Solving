bef804d56a2a7fe351dc9fb24a21e34f90ccd8dc
Chittadeep Biswas <mailchittadeep@gmail.com>
2021-11-24 22:59:55 +0530

    Update relevel.java

--- relevel.java
+++ relevel.java
@@ -119,7 +119,7 @@ class relevel {
             if(k!=-1){
         int p = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (checkIfUncommon(arr, arr[i]) == true) {
+            if (checkIfUncommon(arr, arr[i]) == true && arr[i]>-1) {
                 p = arr[i];
                 break;
             }
